2596bf0c978879d6e63748527f1ef072e926adab
Laabidi-Wafa <laabidiwafa199@gmail.com>
2021-01-18 13:44:25 +0100

    adding the jump up function

--- app.js
+++ app.js
@@ -4,14 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
     function control(e){
         if (e.keyCode === 32) { //the spacebar  https://keycode.info/
         {
-            console.log('pressed') //verification
+            jump()
         }
        
     }
+
     }
     document.addEventListener('keyup', control) //if the keyup event is triggered the control function will roll
 
 
+        function jump(){
+            let position = 0
+            
+
+
+            //move up
+            let timeId = setInterval(function () {
+                console.log('up')
+                position+=30
+                dino.style.bottom = position + 'px'
+
+            },20)
+        }
 
 
 })
