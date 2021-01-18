0a87517e6efa2eb90b79a912f08b4c66a6ff035a
Laabidi-Wafa <laabidiwafa199@gmail.com>
2021-01-18 14:02:35 +0100

    adding the gravity and changing the speed

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const dino = document.querySelector('.dino') //the class dino
     let isJumping = false
-
-
-
+    let gravity = 0.9
 
 
 
@@ -23,24 +21,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     document.addEventListener('keyup', control) //if the keyup event is triggered the control function will roll
 
-
+    let position = 0
         function jump(){
-            let position = 0
-
-           
-            
+            let count = 0
             let timeId = setInterval(function () {
 
                  //move down
-            if (position === 150) {
+            if (count === 150) {
                 clearInterval(timeId)
                 console.log('down')
                 let downIntervalId = setInterval(function() {
-                    if(position === 0 ){
+                    if(count === 0 ){
                         clearInterval(downIntervalId)
                         isJumping = false //after clearing the interval isJumping returns to false so we can hit jump again 
                     }
                     position-=30
+                    count --
+                    position = position * gravity 
                     dino.style.bottom = position + 'px'
                 },20)
                 
@@ -50,8 +47,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //move up
                 console.log('up')
+                count ++
                 position+=30
+                position = position * gravity 
                 dino.style.bottom = position + 'px'
+                console.log(dino.style.bottom)
 
             },20)
         }
